04b47cfba3a1aeb1e9b90633f9fdc35bb35d5f1c
Mdfuad92 <mdfuad98@gmail.com>
2024-04-23 21:20:09 +0600

    server updated

--- index.js
+++ index.js
@@ -46,12 +46,32 @@ async function run() {
       const result = await database.findOne(query)
       res.send(result)
     })
+    app.put('/coffee/:id',async (req,res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert:true }
+      const updatedCoffee  = req.body
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,             
+          quantity: updatedCoffee.quantity,
+          supply: updatedCoffee.supply,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await database.updateOne(filter,coffee,options)
+      res.send(result)
+    })
     app.delete('/coffee/:id',async(req,res)=>{
       const id = req.params.id
       const query = {_id:new ObjectId(id)}
       const result = await database.deleteOne(query)
       res.send(result)
     })
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
